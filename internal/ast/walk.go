2da254dedd90c54eea80c96ea04ea78a3ac35857
ariel <ariellira.al@gmail.com>
2024-07-26 20:46:33 -0700

    possibly using this code for future

---
+++ internal/ast/walk.go
@@ -0,0 +1,212 @@
+package ast
+
+type Visitor interface {
+	Visit(node Node) (v Visitor)
+}
+
+func walkList[N Node](v Visitor, list []N) {
+	for _, node := range list {
+		Walk(v, node)
+	}
+}
+
+func Walk(v Visitor, node Node) {
+	if v := v.Visit(node); v == nil {
+		return
+	}
+
+	switch n := node.(type) {
+	case *SelectStatement:
+		if n.CTE != nil {
+			for _, cte := range *n.CTE {
+				Walk(v, &cte)
+			}
+		}
+		Walk(v, n.SelectBody)
+		break
+	case *SelectBody:
+		Walk(v, n.Top)
+		Walk(v, &n.SelectItems)
+		Walk(v, n.Table)
+		Walk(v, n.WhereClause)
+		Walk(v, n.HavingClause)
+		Walk(v, n.GroupByClause)
+		Walk(v, n.OrderByClause)
+		break
+	case *ExprStringLiteral:
+		break
+	case *ExprNumberLiteral:
+		break
+	case *ExprLocalVariable:
+		break
+	case *ExprIdentifier:
+		break
+	case *ExprQuotedIdentifier:
+		break
+	case *ExprStar:
+		break
+	case *ExprWithAlias:
+		Walk(v, n.Expression)
+		Walk(v, n.Alias)
+		break
+	case *ExprCompoundIdentifier:
+		walkList(v, n.Identifiers)
+		break
+	case *SelectItems:
+		walkList(v, n.Items)
+		break
+	case *WhereClause:
+		Walk(v, n.Clause)
+		break
+	case *HavingClause:
+		Walk(v, n.Clause)
+		break
+	case *GroupByClause:
+		walkList(v, n.Items)
+		break
+	case *TableArg:
+		Walk(v, n.Table)
+		for _, j := range n.Joins {
+			Walk(v, &j)
+		}
+		break
+	case *TableSource:
+		Walk(v, n.Source)
+		break
+	case *Join:
+		Walk(v, n.Table)
+		Walk(v, n.Condition)
+		break
+	case *TopArg:
+		Walk(v, n.Quantity)
+		break
+	case *OrderByArg:
+		Walk(v, n.Column)
+		break
+	case *OrderByClause:
+		for _, e := range n.Expressions {
+			Walk(v, &e)
+		}
+		Walk(v, n.OffsetFetch)
+		break
+	case *OffsetArg:
+		Walk(v, n.Value)
+		break
+	case *FetchArg:
+		Walk(v, n.Value)
+		break
+	case *OffsetFetchClause:
+		Walk(v, &n.Offset)
+		Walk(v, n.Fetch)
+		break
+	case *ExprSubquery:
+		Walk(v, &n.SelectBody)
+		break
+	case *ExprExpressionList:
+		walkList(v, n.List)
+		break
+	case *ExprFunction:
+		Walk(v, n.Name)
+		break
+	case *WindowFrameBound:
+		Walk(v, n.Expression)
+		break
+	case *WindowFrameClause:
+		Walk(v, n.Start)
+		Walk(v, n.End)
+		break
+	case *FunctionOverClause:
+		walkList(v, n.PartitionByClause)
+		for _, o := range n.OrderByClause {
+			Walk(v, &o)
+		}
+		Walk(v, n.WindowFrameClause)
+		break
+	case *ExprFunctionCall:
+		Walk(v, n.Name)
+		walkList(v, n.Args)
+		Walk(v, n.OverClause)
+		break
+	case *ExprCast:
+		Walk(v, n.Expression)
+		break
+	case *CommonTableExpression:
+		Walk(v, n.Columns)
+		Walk(v, &n.Query)
+		break
+	case *DataType:
+		Walk(v, n.DecimalNumericSize)
+		break
+	case *NumericSize:
+		break
+	case *ExprUnaryOperator:
+		Walk(v, n.Right)
+		break
+	case *ExprComparisonOperator:
+		Walk(v, n.Left)
+		Walk(v, n.Right)
+		break
+	case *ExprArithmeticOperator:
+		Walk(v, n.Left)
+		Walk(v, n.Right)
+		break
+	case *ExprAndLogicalOperator:
+		Walk(v, n.Left)
+		Walk(v, n.Right)
+		break
+	case *ExprAllLogicalOperator:
+		Walk(v, n.ScalarExpression)
+		Walk(v, n.Subquery)
+		break
+	case *ExprBetweenLogicalOperator:
+		Walk(v, n.TestExpression)
+		Walk(v, n.Begin)
+		Walk(v, n.End)
+		break
+	case *ExprExistsLogicalOperator:
+		Walk(v, n.Subquery)
+		break
+	case *ExprInSubqueryLogicalOperator:
+		Walk(v, n.TestExpression)
+		Walk(v, n.Subquery)
+		break
+	case *ExprInLogicalOperator:
+		Walk(v, n.TestExpression)
+		walkList(v, n.Expressions)
+		break
+	case *ExprLikeLogicalOperator:
+		Walk(v, n.MatchExpression)
+		Walk(v, n.Pattern)
+		break
+	case *ExprNotLogicalOperator:
+		Walk(v, n.Expression)
+		break
+	case *ExprOrLogicalOperator:
+		Walk(v, n.Left)
+		Walk(v, n.Right)
+		break
+	case *ExprSomeLogicalOperator:
+		Walk(v, n.ScalarExpression)
+		Walk(v, n.Subquery)
+		break
+	case *ExprAnyLogicalOperator:
+		Walk(v, n.ScalarExpression)
+		Walk(v, n.Subquery)
+		break
+	}
+}
+
+type inspector func(Node) bool
+
+func (f inspector) Visit(node Node) Visitor {
+    if f(node) {
+        return f
+    }
+
+    return nil
+}
+
+// traverse tree depth first
+func Inspect(node Node, f func(Node) bool){
+    Walk(inspector(f), node)
+}
