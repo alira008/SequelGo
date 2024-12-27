5e7fa781821417b3796f01e7ee64c5fb85e877d3
Ariel Lira <ariellira.al@gmail.com>
2024-12-27 12:29:01 -0800

    switch to production zap

--- cmd/SequelGo-formatter/main.go
+++ cmd/SequelGo-formatter/main.go
@@ -57,7 +57,7 @@ func validStringEnums(cmd *cobra.Command, args []string) error {
 }
 
 func runFormatter(cmd *cobra.Command, args []string) {
-	logger, _ := zap.NewDevelopment()
+	logger, _ := zap.NewProduction()
 	defer logger.Sync()
 	sugar := logger.Sugar()
 
